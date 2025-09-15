88dfe05b088fd4f12e32255fc9109af7ac36589b
Jeffy Jefferson <jeffyj080@gmail.com>
2025-09-15 14:02:07 +0200

    Messing around some more. Displaying multiple components at once.

--- react_hello_world/src/Card.jsx
+++ react_hello_world/src/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Card(props){
     return (
       <div>
-          <h1>{props.Title}</h1>
-            <p>
+            <h1 style={{color : "black"}}>{props.Title}</h1>
+            <p style={{color : "navy"}}>
                 {props.Content}
             </p>
         </div>
@@ -13,10 +13,10 @@ function Card(props){
 
 function Deck() {
     return (
-        <>
+        <div style={{backgroundColor : "beige"}}>
             <Card Title="Card 1" Content="Ace"/>
             <Card Title="Card 2" Content="Ace" />
-        </>
+        </div>
     )
 }
 
